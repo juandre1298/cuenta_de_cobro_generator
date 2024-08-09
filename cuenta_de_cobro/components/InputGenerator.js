cfe272d5b571dd1c749102b68e6e3d8c3d97cc38
juan16 <juan@speeddigital.com>
2024-08-09 17:16:33 -0500

    add scss to app

---
+++ cuenta_de_cobro/components/InputGenerator.js
@@ -0,0 +1,14 @@
+"use client"
+import React, { useState, useEffect } from 'react';
+
+
+const InputGenerator = ({  }) => {
+  
+  return (
+    <div>
+      input generator
+    </div>
+  );
+};
+
+export default InputGenerator;
--- cuenta_de_cobro/pages/index.js
+++ cuenta_de_cobro/pages/index.js
@@ -2,7 +2,10 @@
 import { useEffect, useState } from 'react';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import globalStyle from "../styles/global.module.scss"
 import CuentaDeCobro from '/components/CuentaDeCobro';
+import InputGenerator from '../components/InputGenerator';
+
 
 export default function Home() {
   const [ files, useFiles ] = useState([]);
@@ -20,15 +23,27 @@ export default function Home() {
   },[])
 
   return (
-    <div className={styles.container}>
+    <div className={globalStyle.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
+        <div >
+          <div className='input-section'>  
+            <label className={globalStyle.title} >Algun formato en especifico?</label>
+            <input type="file" onChange={handleFilesUpload} />
+            <InputGenerator/>
+          </div>
+          <div className='result-section'>
+            <div className='hoja'>
+              <CuentaDeCobro files={files}/>
+            </div>
+          </div>
+        </div>
 
-        <input type="file" onChange={handleFilesUpload} />
-        <CuentaDeCobro files={files}/>
+ 
+       
       </main>
     </div>
   );
